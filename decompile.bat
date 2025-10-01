39ac5b8d3aef8532dda5a47add1d654383c7f1e5
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-10-01 20:30:38 +0200

    Update decompile.bat

--- decompile.bat
+++ decompile.bat
@@ -1,12 +1,20 @@
 @echo off
-REM Decompile StanfordJS.jar using CFR
-REM Make sure cfr-0.152.jar and StanfordJS.jar are in the same folder.
+REM Decompile StanfordJS.jar using CFR and zip the sources
+REM Place this script in the same folder as cfr-0.152.jar and StanfordJS.jar
 
 set OUT_DIR=StanfordJS_src
-if not exist %OUT_DIR% mkdir %OUT_DIR%
+set ZIP_FILE=StanfordJS_src.zip
+
+REM clean old output
+if exist %OUT_DIR% rmdir /s /q %OUT_DIR%
+if exist %ZIP_FILE% del %ZIP_FILE%
+mkdir %OUT_DIR%
 
 echo Running CFR decompiler...
 java -jar cfr-0.152.jar StanfordJS.jar --outputdir %OUT_DIR%
 
-echo Decompilation complete. Sources are in %OUT_DIR%\
+echo Zipping sources...
+powershell -command "Compress-Archive -Path %OUT_DIR% -DestinationPath %ZIP_FILE%"
+
+echo Done! Decompiled sources in %OUT_DIR%\ and also in %ZIP_FILE%
 pause
